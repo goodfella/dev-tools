e9b18595089490f0a7c8df99b6e271c3d9c288a7
nick <goodfella005@gmail.com>
2012-11-10 17:10:41 -0800

    emacs/my-git.el:  my-git-show improvements
    * Checks to insure the path is controled by a git repository

--- emacs/my-git.el
+++ emacs/my-git.el
@@ -3,8 +3,8 @@
   (let ((dirpath (expand-file-name (file-truename dirpath))))
     (when (file-directory-p dirpath)
       (let* ((default-directory (file-name-as-directory dirpath))
-	     (gitdir (shell-command-to-string "git rev-parse --show-cdup | tr -d '\n'")))
-	(when gitdir
+	     (gitdir (shell-command-to-string "git rev-parse --show-cdup 2> /dev/null | tr -d '\n'")))
+	(when (not (string= gitdir ""))
 	  (expand-file-name gitdir dirpath))))))
 
 (defun my-git-show ()
@@ -16,6 +16,8 @@
        (gittopdir (my-git-top-dir (file-name-directory path)))
        (relpath (file-relative-name path gittopdir))
        (buffername (format "%s:%s" relpath committish)))
-    (let* ((default-directory gittopdir))
-      (call-process "git" nil buffername t "--no-pager" "show" (format "%s:%s" committish relpath))
-      (switch-to-buffer buffername))))
+    (if gittopdir
+	(let* ((default-directory gittopdir))
+	  (call-process "git" nil buffername t "--no-pager" "show" (format "%s:%s" committish relpath))
+	  (switch-to-buffer buffername))
+      (message (format "path: %s, is not under git's control" path)))))
