1c991c1b0612c326a3508a8cbf4a196a292a3da4
nick <goodfella005@gmail.com>
2013-04-13 13:56:37 -0700

    New lisp file for gtags stuff

---
+++ emacs/my-gtags.el
@@ -0,0 +1,10 @@
+(defun my-add-gtags-path ()
+  "Adds a gtags path to GTAGSLIBPATH"
+  (interactive)
+  (let
+      ((path (file-name-as-directory (expand-file-name (read-directory-name "gtags-path: "))))
+       (gtagslibpath (getenv "GTAGSLIBPATH")))
+    (setenv "GTAGSLIBPATH"
+	    (if (> (length gtagslibpath) 0)
+		(mapconcat (lambda (s) (format "%s" s)) (delete-dups (append (make-list 1 path) (parse-colon-path gtagslibpath))) (path-separator))
+	      path))))
